08a954693452d2f4768edc09bd4b052569ffb01e
lesley-byte <phosphorescence3@hotmail.com>
2023-01-21 12:56:35 -0500

    got server to start now to deal with EagerLoadingError

--- controllers/index.js
+++ controllers/index.js
@@ -1,20 +1,9 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./api');
-const homeRoutes = require('./home-routes');
-const categoryRoutes = require('./category-routes');
-const commentRoutes = require('./comment-routes');
-const tagsRoutes = require('./tag-routes');
-const userRoutes = require('./user-routes');
-
-// API Routes
+const homeRoutes = require('./home-routes.js');
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
-router.use('/categoryRoutes', categoryRoutes);
-router.use('/commentRoutes', commentRoutes);
-router.use('/reviewRoutes'), reviewRoutes;
-router.use('/tagRoutes', tagsRoutes);
-router.use('/users', userRoutes);
 
 module.exports = router;
