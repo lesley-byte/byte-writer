b24bee96f6d77d7078cf99e1b5d510e137431b0d
lesley-byte <phosphorescence3@hotmail.com>
2023-01-22 12:24:43 -0500

    Starting the README.md file

--- controllers/index.js
+++ controllers/index.js
@@ -2,8 +2,16 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes.js');
+const categoryRoutes = require('./category-routes.js');
+const commentRoutes = require('./comment-routes.js');
+const reviewRoutes = require('./review-routes.js');
+const tagRoutes = require('./tag-routes.js');
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
+router.use('/categories', categoryRoutes);
+router.use('/comments', commentRoutes);
+router.use('/reviews', reviewRoutes);
+router.use('/tags', tagRoutes);
 
 module.exports = router;
--- controllers/review-routes.js
+++ controllers/review-routes.js
@@ -1,72 +1,72 @@
-// const router = require('express').Router();
-// const { User, Review, Category, Comment } = require('../models');
+const router = require('express').Router();
+const { User, Review, Category, Comment } = require('../models');
 
-// // GET all reviews
-// router.get('/', async (req, res) => {
-//   try {
-//     const dbReviewData = await Review.findAll({
-//       include: [
-//         {
-//           model: User,
-//           attributes: ['username'],
-//         },
-//         {
-//           model: Category,
-//           attributes: ['category_name'],
-//         },
-//         {
-//           model: Comment,
-//           attributes: ['comment_text', 'user_id', 'review_id'],
-//           include: {
-//             model: User,
-//             attributes: ['username'],
-//           },
-//         },
-//       ],
-//     });
-//     const reviews = dbReviewData.map((review) => review.get({ plain: true }));
-//     res.render('homepage', {
-//       reviews,
-//       loggedIn: req.session.loggedIn,
-//     });
-//   } catch (err) {
-//     console.log(err);
-//     res.status(500).json(err);
-//   }
-// });
+// GET all reviews
+router.get('/reviews', async (req, res) => {
+  try {
+    const dbReviewData = await Review.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['username'],
+        },
+        {
+          model: Category,
+          attributes: ['category_name'],
+        },
+        {
+          model: Comment,
+          attributes: ['comment_text', 'user_id', 'review_id'],
+          include: {
+            model: User,
+            attributes: ['username'],
+          },
+        },
+      ],
+    });
+    const reviews = dbReviewData.map((review) => review.get({ plain: true }));
+    res.render('homepage', {
+      reviews,
+      loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
 
-// // GET one review
-// router.get('/review/:id', async (req, res) => {
-//   try {
-//     const dbReviewData = await Review.findByPk(req.params.id, {
-//       include: [
-//         {
-//           model: User,
-//           attributes: ['username'],
-//         },
-//         {
-//           model: Category,
-//           attributes: ['category_name'],
-//         },
-//         {
-//           model: Comment,
-//           attributes: ['comment_text', 'user_id', 'review_id'],
-//           include: {
-//             model: User,
-//             attributes: ['username'],
-//           },
-//         },
-//       ],
-//     });
-//     const review = dbReviewData.get({ plain: true });
-//     res.render('review', {
-//       review,
-//       loggedIn: req.session.loggedIn,
-//     });
-//   } catch (err) {
-//     console.log(err);
-//     res.status(500).json(err);
-//   }
-// });
+// GET one review
+router.get('/reviews/:id', async (req, res) => {
+  try {
+    const dbReviewData = await Review.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ['username'],
+        },
+        {
+          model: Category,
+          attributes: ['category_name'],
+        },
+        {
+          model: Comment,
+          attributes: ['comment_text', 'user_id', 'review_id'],
+          include: {
+            model: User,
+            attributes: ['username'],
+          },
+        },
+      ],
+    });
+    const review = dbReviewData.get({ plain: true });
+    res.render('review', {
+      review,
+      loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
 
-// module.exports = router;
+module.exports = router;
