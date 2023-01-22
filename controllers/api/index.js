499a17b32a6f4dde6708033355f71da249ba26b9
lesley-byte <phosphorescence3@hotmail.com>
2023-01-21 22:43:34 -0500

    updated route for users so that signup would work

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,6 +2,6 @@ const router = require('express').Router();
 // also require the routes from the api folder
 const userRoutes = require('./user-routes'); // this is the user-routes.js file
 // and add them to the router with router.use()
-router.use('/userRoutes', userRoutes);
+router.use('/users', userRoutes);
 
 module.exports = router;
